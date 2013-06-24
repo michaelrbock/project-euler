4bf88ceac07d9867b6e0a0757bdce5ed0ef93967
Michael Bock <mykel.bock@gmail.com>
2013-06-23 17:03:02 -0700

    problem 7

--- p7.py
+++ p7.py
@@ -1,2 +1,18 @@
 def is_prime(num):
-	
+	for i in range(2, num):
+		if num % i == 0:
+			return False
+	return True
+
+count = 1
+number = 3
+
+while True:
+	if is_prime(number):
+		count += 1
+		if count == 10001:
+			break
+	number += 1
+
+
+print number
