20997b8ca4c8e229861dabea539c60550182e70e
Michael Bock <mykel.bock@gmail.com>
2013-06-23 19:42:52 -0700

    wayyyyy better p10

--- p10.py
+++ p10.py
@@ -1,5 +1,14 @@
+import math
+
 def is_prime(num):
-	for i in range(2, num):
+	if num <= 1:
+		return False
+	if num == 2:
+		return True
+	if num % 2 == 0:
+		return False
+	m = int(math.sqrt(num))
+	for i in range(3, m+1, 2):
 		if num % i == 0:
 			return False
 	return True
